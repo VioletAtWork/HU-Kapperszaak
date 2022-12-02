9795e434da60e1156ec974f5fe28034fa9a605d5
Menno van Wijk <mennovanwijk.dev@gmail.com>
2022-12-02 13:21:14 +0100

    2nd commit. NOT a stable version. Made start of project architecture incl. package config & home page

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+const Footer = () => {
+    return (
+        <Box align="center" opacity={0.4} fontSize="sm">
+            &copy; Made by "One-Direction" Squad.
+        </Box>
+    )
+}
+
+export default Footer
