f10808e9a5a96dbbd11db024a5bbcf7cd8241245
Owens <owens.holliman@student.hu.nl>
2023-01-09 10:31:16 +0100

    nodes installeren en verwijderen

--- .next/static/webpack/webpack.95fdb2ac5e4a3728.hot-update.js
+++ .next/static/webpack/webpack.95fdb2ac5e4a3728.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3bc97036a0ca342e"; }
+/******/ 	__webpack_require__.h = function() { return "bdcb2cb3dc5fb3d7"; }
 /******/ }();
 /******/ 
 /******/ }
