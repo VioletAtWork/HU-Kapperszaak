0a0a111c26304a51eb4d3003ba8a7d9e7df7b039
Roalddekwant <115426369+Roalddekwant@users.noreply.github.com>
2023-01-12 15:07:14 +0100

    next stuff

--- .next/static/webpack/webpack.6e4e5baf44c11762.hot-update.js
+++ .next/static/webpack/webpack.6e4e5baf44c11762.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5282d81d5e234dba"; }
+/******/ 	__webpack_require__.h = function() { return "8a59a82109a1842c"; }
 /******/ }();
 /******/ 
 /******/ }
