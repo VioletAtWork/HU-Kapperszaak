937bdf6cd71bc831be9c2b813fefe220f136ee05
Owens <owens.holliman@student.hu.nl>
2023-01-13 09:55:37 +0100

    next changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38ea401cccdb7a0c"; }
+/******/ 		__webpack_require__.h = function() { return "538926eb4d74a797"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
