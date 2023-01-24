738b34ed3f3314c9dabb277477829a879c54836e
Twinkel5 <118807915+Twinkel5@users.noreply.github.com>
2023-01-24 12:26:49 +0100

    Revert "Merge branch 'feature/works' of https://github.com/VioletAtWork/HU-Kapperszaak into feature/works"
    This reverts commit 21472599626a922d0d17c28cf06e8e0f77ac93df, reversing
    changes made to 4a26c98fb341892c8c8d45773d8417431f207606.

--- components/navbar.js
+++ components/navbar.js
@@ -37,6 +37,9 @@ const Navbar = props => {
                             Een afspraak maken
                         </Button>
                     </NextLink>
+
+
+
                 </HStack>
 
                 <Flex align="center" ml="auto"
