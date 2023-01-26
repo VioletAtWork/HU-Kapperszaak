64e81a2b03b926d4803706072aa8c653a3ef626a
Owens <owens.holliman@student.hu.nl>
2023-01-26 10:16:31 +0100

    next garbage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62fc30c6b9941403"; }
+/******/ 		__webpack_require__.h = function() { return "6f24619dee3bd444"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
